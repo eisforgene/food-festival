1a6c6339e11ff781077f4e924c72c663d5a1fc06
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-10-19 14:22:22 -0700

    serviceworker troubleshoot

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const APP_PREFIX = 'FoodFest-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js",
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil( // tells browser to wait until the work is complete before terminating service worker
+        caches.open(CACHE_NAME).then(function (cache) { // caches.open finds specific cache by name and then adds every file
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
